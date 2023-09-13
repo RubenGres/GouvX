3f2dd12c006ad2874456655d9bb6a5d81b23dd34
Retronyme <rubengres@hotmail.fr>
2023-09-13 13:13:49 +0000

    Updated scraper and parse list without crawler

--- gouvx_scraper.py
+++ gouvx_scraper.py
@@ -2,7 +2,126 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-class BasicScraper:
+def parse_table(table):
+    output = "\n\n"
+    rows = table.find_all('tr')
+
+    for i, row in enumerate(rows):
+        cells = row.find_all(['th', 'td'])
+        cell_texts = [cell.get_text(strip=True) for cell in cells]
+
+        if i == 0:
+            output += "|" + "|".join(cell_texts) + "|\n"  # Header row
+            output += "|" + "|".join(["---"] * len(cells)) + "|\n"  # Separator row
+        else:
+            output += "|" + "|".join(cell_texts) + "|\n"  # Data rows
+        
+    return output
+
+
+def parse_link(link):
+    link_text = link.get_text()
+    link_url = link.get('href')
+    return f'[{link_text}]({link_url})'
+
+
+def parse_element(element):
+    # Initialize an empty string to store the result
+    output = ""
+
+    elements = [element] if not isinstance(element, list) else element
+
+    for elem in elements:
+        if hasattr(elem, "children"):
+            for child in elem.children:
+                if child.name and len(child.name) == 2 and child.name.startswith('h'):
+                    header_level = int(child.name[1])
+                    output += f'\n\n{"#"*header_level} {parse_element(child)}:\n'
+                elif child.name == 'li':
+                    output += f'\n- {parse_element(child)}'
+                elif child.name == 'p':
+                    output += f'\n{parse_element(child)}'
+                elif child.name == 'table':
+                    output += parse_table(child)
+                elif child.name == 'a':
+                    output += parse_link(child)
+                else:
+                    output += f'{parse_element(child)}'
+        else:
+            return f'{elem.get_text()}'
+    
+    return output
+
+
+
+def get_tabs(soup):
+    main_div = soup.find('main', id='main')
+    article = main_div.find('article')
+    fiche = article.find('div', id="expired", attrs={"data-test": "div-content-fiche"})
+    
+    tabs = fiche.select('[id^="main-tabs-situation-content-"], [data-toggle-scope-seeall="chapters"]')
+    if not tabs:
+        text_content = fiche.find_all("p", attrs={"data-test": "contenu-texte"})
+        return zip(['']*len(text_content), text_content)
+
+    tab_header = fiche.find(class_="fr-tabs__list")
+    if tab_header:
+        tab_names = [f'#{name.get_text()}\n\n' for name in tab_header.find_all('li')]
+        return zip(tab_names, tabs)
+    else:
+        return zip([''], tabs)
+
+
+
+def parse_tabs(tab):
+    output = ""
+    
+    chapters = tab.find_all('div', class_="sp-chapter")
+
+    if not chapters:
+        output += parse_element(tab)
+        output += '\n\n\n'
+    else:
+        for chapter in chapters:
+            output += parse_chapter(chapter)
+            output += '\n\n\n'
+
+    return output
+
+
+def get_metadata(soup):
+    main_div = soup.find('main', id='main')
+    nav = main_div.find('nav').find('ol').find_all('li')
+    breadcrums = [n.get_text() for n in nav]
+
+    article = main_div.find('article')
+
+    title = article.find('h1').get_text()
+    date_verif = article.find('p',  attrs={"data-test": "date-maj"}).get_text().split(' - ')[0]
+    
+    intro = article.find('div', id='intro')
+    intro = parse_element(intro) if intro else None
+
+    return {
+        "breadcrums": breadcrums,
+        "title": title,
+        "date_verif": date_verif,
+        "intro": intro
+    }
+
+def parse_chapter(chapter):
+    output = ""
+
+    chapter_title = chapter.find(class_='sp-chapter-title').get_text()
+    output += f'##{chapter_title}\n'
+
+    chapter_content = chapter.find("div", class_="sp-chapter-content")
+    output += parse_element(chapter_content)
+
+    return output
+
+
+class ServicePublicScraper:
     def __init__(self, response):
         self.response = response
         self.soup = BeautifulSoup(response.text, 'html.parser')
@@ -19,8 +138,14 @@ class BasicScraper:
     
 
     def scrape_text(self):
-        text_content = self.soup.get_text()
-        return text_content
+        output = ""
+
+        for tab_title, tab_content in get_tabs(self.soup):
+            output += f'{tab_title}'
+            for tab_text in parse_tabs(tab_content):
+                output += tab_text
+
+        return output
     
 
     def scrape_links(self, include=None, exclude=None):
@@ -66,10 +191,15 @@ class BasicScraper:
         return response.headers
 
     def parse_page(self):
+        metadata = get_metadata(self.soup)
+
         return {
             "url": self.url,
+            "title": metadata["title"],
+            "breadcrums": metadata["breadcrums"],
+            "intro": metadata["intro"],
+            "date_verif": metadata["date_verif"],
             "text": self.scrape_text(),
-            #"body": self.response.text,
             "images": self.scrape_img_url(),
             "links": self.scrape_links()
         }
---
+++ scrape_from_urls.py
@@ -0,0 +1,34 @@
+import click
+from tqdm import tqdm
+from gouvx_scraper import ServicePublicScraper
+import requests
+import os
+import json
+
+@click.command()
+@click.option('--list', '-l', help='Path of the list of websites to scrape file')
+def scrape_links(list):
+
+    with open(list, 'r') as list_file:
+        urls = list_file.readlines()
+    
+    for url in tqdm(urls):
+        url = url.strip()
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            try:
+                scraper = ServicePublicScraper(response)
+                page_content = scraper.parse_page()
+                path = os.path.join("data_scraped", f"{url.split('/')[-1]}.json")
+
+                with open(path, 'w') as f:
+                    f.write(json.dumps(page_content))
+            except:
+                print(url, "HAD AN EXCEPTION")
+        else:
+            print(url, "ERROR ON REPONSE CODE")
+
+if __name__ == '__main__':
+    scrape_links()
